10247edc129e4248f27c808d88bb2825553355f3
Manjot Singh <manjotspawra@gmail.com>
2025-11-07 10:20:09 +0100

    Renamed file and created the browser instance

---
+++ test_open_browser.py
@@ -0,0 +1,10 @@
+from playwright.sync_api import sync_playwright 
+
+
+def test_open_browser():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto("https://www.saucedemo.com")
+        page.wait_for_timeout(10000)
+        browser.close()        
